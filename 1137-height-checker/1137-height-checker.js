ef2944941ac566462ffd9acee046e0865a02f74e
a1dana04 <150905725+a1dana04@users.noreply.github.com>
2024-08-24 17:57:53 +0600

    Time: 44 ms (95.80%), Space: 49052000 (60.27%) - LeetHub

--- 1137-height-checker/1137-height-checker.js
+++ 1137-height-checker/1137-height-checker.js
@@ -3,13 +3,13 @@
  * @return {number}
  */
 var heightChecker = function(heights) {
-    let heightt = heights.slice().sort((a,b)=> a-b)
-     let count = 0
-    for( let i = 0; i < heights.length; i++ ){
-        if(heights[i] !== heightt[i]){
-            count++
-        } 
+   let expected = heights.slice().sort((a,b)=>a-b)
+   let count = 0
+   for(let i = 0; i< heights.length; i++){
+    if(heights[i] !== expected[i]){
+        count++
     }
-    return count
-    
+   }
+   return count
+
 };
