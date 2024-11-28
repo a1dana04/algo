104c0f30c2881c9cae39d6c552938e8a282d89f5
Aidana <150905725+a1dana04@users.noreply.github.com>
2024-11-28 14:19:14 +0600

    Time: 0 ms (100.00%), Space: 49336000 (77.55%) - LeetHub

---
+++ 0043-multiply-strings/0043-multiply-strings.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
+var multiply = function(num1, num2) {
+       let res3 = BigInt(num1);
+  let res4 = BigInt(num2);
+  let res5 = res3 * res4;
+  return res5 + "";
+    
+};
